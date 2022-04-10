acba40ab0714099038c7482c062e01b9b6d762a9
jiyong45 <jiyong90145@gmail.com>
2022-04-10 15:17:29 +0900

    변수선언의 val 과 var 의 차이점 개념 다시 잡기

--- app/src/main/java/com/example/android_basic/MainActivity.kt
+++ app/src/main/java/com/example/android_basic/MainActivity.kt
@@ -7,5 +7,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //1. val 의 선언
+        val value = "value"
+        //value = "불변함 "  <-- Value는 값의 변경이 불가능함
+
+        //2. var 의 선언
+        var variable = "2022.04.10"
+        variable = "variable 의 경우 이렇게 값의 변경이 가능하다"
+
+
+
+
+
+
     }
 }
