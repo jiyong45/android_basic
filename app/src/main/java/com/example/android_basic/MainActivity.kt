363d71531b42a72b21c4d1db57509c06a6968d47
jiyong45 <jiyong90145@gmail.com>
2022-04-10 15:55:47 +0900

    value , variable ,lateinit , by lazy 차이점 확인 예제
    수많은 정보가 쏟아지는중... 많이 공부해야겠다...

--- app/src/main/java/com/example/android_basic/MainActivity.kt
+++ app/src/main/java/com/example/android_basic/MainActivity.kt
@@ -9,6 +9,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+    }
+
+    fun var_val(){
         //1. val 의 선언
         val value = "value"
         //value = "불변함 "  <-- Value는 값의 변경이 불가능함
@@ -16,11 +20,33 @@ class MainActivity : AppCompatActivity() {
         //2. var 의 선언
         var variable = "2022.04.10"
         variable = "variable 의 경우 이렇게 값의 변경이 가능하다"
+        println(variable)
+    }
+
+    fun lateInit() {
+        lateinit var text: String
+
+        val result1 = 30
+
+        text = "Result : $result1"
+        println(text)
+
+        val result2 = 50
+
+        text = "Result : ${result1 + result2}"
+        println(text)
+
+    }
 
 
+    fun by_Lazy() {
+        lateinit var text: String
+        val textLength: Int by lazy {
+            text.length
+        }
 
-
-
-
+        // 대충 중간에 뭔가 했음
+        text = "H43RO_Velog"
+        println(textLength)
     }
 }
